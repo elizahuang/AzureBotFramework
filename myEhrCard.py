c20d68497c2ec4923a69108eca9638ff1d67ff9c
elizahuang <eliza85827@gmail.com>
2021-08-03 07:05:25 +0000

    fill disappear items

--- myEhrCard.py
+++ myEhrCard.py
@@ -18,7 +18,7 @@ card={
     "actions": [
         {
             "type": "Action.ShowCard",
-            "title": "類別三",
+            "title": "類別一",
             "card": {
                 "type": "AdaptiveCard",
                 "version": "1.3",
@@ -61,8 +61,8 @@ card={
                     ],
                     "height": "stretch",
                     "separator": True
-                },
-                                    {
+                    },
+                    {
                     "type": "ColumnSet",
                     "columns": [
                         {
@@ -100,52 +100,16 @@ card={
                     ],
                     "height": "stretch",
                     "separator": True
-                },
-                                    {
-                    "type": "ColumnSet",
-                    "columns": [
-                        {
-                            "type": "Column",
-                            "width": "stretch",
-                            "items": [
-                                {
-                                    "type": "ActionSet",
-                                    "actions": [
-                                        {
-                                            "type": "Action.OpenUrl",
-                                            "title": "Google1",
-                                            "url": "https://www.google.com/",
-                                            "style": "positive"
-                                        }
-                                    ],
-                                    "height": "stretch"
-                                }
-                            ],
-                            "style": "accent",
-                            "height": "stretch",
-                            "verticalContentAlignment": "Center"
-                        },
-                        {
-                            "type": "Column",
-                            "width": "stretch",
-                            "items": [
-                                {
-                                    "type": "TextBlock",
-                                    "text": "description,description,description,description,description,description,description,description,description,description",
-                                    "wrap": True
-                                }
-                            ]
-                        }                        
-                    ],
-                    "height": "stretch",
-                    "separator": True
-                }    
-            ]
+                    }   
+                ]
             },
             "style": "default"
         }
     ]
 }
+
+
+
 card_old={
     "type": "AdaptiveCard",
     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
@@ -226,93 +190,21 @@ card_old={
     ]
 }
 
-card2={
-    "type": "AdaptiveCard",
-    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "version": "1.3",
-    "body": [
-        {
-            "type": "TextBlock",
-            "size": "Medium",
-            "weight": "Bolder",
-            "text": "My Ehr 快捷儀表板",
-            "horizontalAlignment": "Center",
-            "height": "stretch",
-            "separator": True,
-            "spacing": "Medium"
-        }
-    ],
-    "actions": [
-        {
-            "type": "Action.ShowCard",
-            "title": "類別一",
-            "card": {
-                "type": "AdaptiveCard",
-                "actions": [
-                    {
-                        "type": "Action.OpenUrl",
-                        "title": "View",
-                        "url": "https://www.google.com/"
-                    },
-                    {
-                        "type": "Action.OpenUrl",
-                        "title": "View",
-                        "url": "https://tw.yahoo.com/"
-                    }
-                ]
-            },
-            "style": "default"
-        },
-        {
-            "type": "Action.ShowCard",
-            "title": "類別二",
-            "card": {
-                "type": "AdaptiveCard",
-                "actions": [
-                    {
-                        "type": "Action.OpenUrl",
-                        "title": "View",
-                        "url": "https://www.google.com/"
-                    },
-                    {
-                        "type": "Action.OpenUrl",
-                        "title": "View",
-                        "url": "https://tw.yahoo.com/"
-                    }
-                ]
-            },
-            "style": "default"
-        },
-        {
-            "type": "Action.ShowCard",
-            "title": "類別三",
-            "card": {
-                "type": "AdaptiveCard",
-                "actions": [
-                    {
-                        "type": "Action.OpenUrl",
-                        "title": "View",
-                        "url": "https://www.google.com/",
-                        "style": "positive"
-                    },
-                    {
-                        "type": "Action.OpenUrl",
-                        "title": "View",
-                        "url": "https://tw.yahoo.com/",
-                        "style": "positive"
-                    }
-                ]
-            },
-            "style": "default"
-        }
-    ]
-}
+def prepareEhrCard(infos):
+    infos=[{"name": "Google","url":"https://www.google.com/","category":"cat 1", "description":"description,description,description,description,description,description"},
+    {"name": "Google2","url":"https://www.google.com/","category":"cat 2", "description":"description,description,description,description,description,description"},
+    {"name": "Google2","url":"https://www.google.com/","category":"cat 2", "description":"description,description,description,description,description,description"},
+    {"name": "Google2","url":"https://www.google.com/","category":"cat 2", "description":"description,description,description,description,description,description"},
+    {"name": "Google3","url":"https://www.google.com/","category":"cat 3", "description":"description,description,description,description,description,description"}    ]
+        
+    sortedInfoDict={}
+    for info in infos:
+        category=info["category"]
+        del info["category"]
+        sortedInfoDict[category]=sortedInfoDict[category]+[info] if category in sortedInfoDict.keys() else [info]
 
-def prepareEhrCard(choice):
-    # myEhrCard=copy.deepcopy(card)
-    if choice==1:
-        myEhrCard=copy.deepcopy(card)
-    else: 
-        myEhrCard=copy.deepcopy(card2)
+    print('sortedInfoDict\n',sortedInfoDict)  
 
+
+    myEhrCard=copy.deepcopy(card)
     return myEhrCard
