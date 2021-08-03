1a8cb7355e677da160ac240c9d5de622837289f4
elizahuang <eliza85827@gmail.com>
2021-08-03 01:54:47 +0000

    debug connecting api

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ class MyBot(ActivityHandler):
                                 'todo_contents': turn_context.activity.value['todo_contents'],
                                 'todo_completed': turn_context.activity.value['todo_completed'],
                                 'todo_update_date': turn_context.activity.timestamp.strftime("%Y/%m/%d"),
-                                'user_id': turn_context.activity.channel_data['tenant']['id'],
+                                'employee_id': '120734'#turn_context.activity.channel_data['tenant']['id'],
                                 }
                                 # 'employee_id': turn_context.activity.channel_data['tenant']['id'],
                                 # "line_user_id": turn_context.activity.channel_data['tenant']['id'],    #delete
