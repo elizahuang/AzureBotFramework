43e3ab6272101d805365e3df257cee44c5bc95b2
elizahuang <yiiiihuang@gmail.com>
2021-08-04 05:42:49 +0000

    delete file not used

--- bot.py
+++ bot.py
@@ -154,12 +154,13 @@ class MyBot(ActivityHandler):
 
                 elif turn_context.activity.value =='confirm_delete_task':
                     data=turn_context.activity.value
-                    # singletask ={"todo_id":data["todo_id"]}
+                    taskId=data["todo_id"]
                     requests.delete(f'https://tsmcbot-404notfound.du.r.appspot.com/api/todo/%s/%s'%(teams_tenantID,data["todo_id"]))#,json=singletask
-                    contextToReturn='Todo List 項目ID: '+data["todo_id"]+' 資料成功刪除'
+                    contextToReturn='Todo List 項目ID: '+taskId+' 資料成功刪除'
                 elif turn_context.activity.value =='cancel_delete_task':
                     data=turn_context.activity.value
-                    contextToReturn='Todo List 項目ID: '+data["todo_id"]+' 資料未刪除'
+                    taskId=data["todo_id"]
+                    contextToReturn='Todo List 項目ID: '+taskId+' 資料未刪除'
                         
                 elif turn_context.activity.value['card_request_type'] == 'submit_update':
                     data=turn_context.activity.value
